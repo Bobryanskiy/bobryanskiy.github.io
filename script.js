4117022d0afecb28df6b2fcc9335f6e202a994cc
Bobryanskiy <cyxov04@gmail.com>
2025-03-15 03:37:21 +0300

    f

--- script.js
+++ script.js
@@ -2,36 +2,37 @@ const button = document.querySelector(".theme-toggle");
 const body = document.querySelector(".container");
 
 button.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.slider-img');
+    const leftButton = document.querySelector('.slider-btn-left');
+    const rightButton = document.querySelector('.slider-btn-right');
 
-const images = document.querySelectorAll('.slider-img');
-const leftButton = document.querySelector('.slider-btn-left');
-const rightButton = document.querySelector('.slider-btn-right');
+    let currentIndex = 0;
 
-let currentIndex = 0;
+    function showImage(index) {
+        images.forEach(img => {
+            img.classList.remove('active');
+        });
 
-function showImage(index) {
-    images.forEach(img => {
-        img.classList.remove('active');
+        images[index].classList.add('active');
+    }
+
+    leftButton.addEventListener('click', () => {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = images.length - 1;
+        }
+        showImage(currentIndex);
+    });
+    rightButton.addEventListener('click', () => {
+        currentIndex++;
+        if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+        showImage(currentIndex);
     });
 
-    images[index].classList.add('active');
-}
-
-leftButton.addEventListener('click', () => {
-    currentIndex--;
-    if (currentIndex < 0) {
-        currentIndex = images.length - 1;
-    }
     showImage(currentIndex);
-});
-rightButton.addEventListener('click', () => {
-    currentIndex++;
-    if (currentIndex >= images.length) {
-        currentIndex = 0;
-    }
-    showImage(currentIndex);
-});
-
-showImage(currentIndex);
+})
